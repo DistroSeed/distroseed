660abd4a7c883b9cf3350b9bfbdecd314840fe27
Ian Norden <iancnorden@gmail.com>
2015-07-12 22:07:15 -0400

    Settings updates
    Fixing a lot of first tab in settings, slim down and less panes

--- js/three-button-handler.js
+++ js/three-button-handler.js
@@ -15,3 +15,31 @@ $( "#buttonselectrequiredencryption" ).click(function() {
   $("#buttonselectpreferredencryption").attr("class", "btn btn-default");
   $("#buttonselectrequiredencryption").attr("class", "btn btn-default active");
 });
+
+$( "#transmissionlogginglevelbuttonnone" ).click(function() {
+  $("#transmissionlogginglevelbuttonnone").attr("class", "btn btn-default active");
+  $("#transmissionlogginglevelbuttonerror").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttoninfo").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttondebug").attr("class", "btn btn-default");
+});
+
+$( "#transmissionlogginglevelbuttonerror" ).click(function() {
+  $("#transmissionlogginglevelbuttonnone").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttonerror").attr("class", "btn btn-default active");
+  $("#transmissionlogginglevelbuttoninfo").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttondebug").attr("class", "btn btn-default");
+});
+
+$( "#transmissionlogginglevelbuttoninfo" ).click(function() {
+  $("#transmissionlogginglevelbuttonnone").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttonerror").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttoninfo").attr("class", "btn btn-default active");
+  $("#transmissionlogginglevelbuttondebug").attr("class", "btn btn-default");
+});
+
+$( "#transmissionlogginglevelbuttondebug" ).click(function() {
+  $("#transmissionlogginglevelbuttonnone").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttonerror").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttoninfo").attr("class", "btn btn-default");
+  $("#transmissionlogginglevelbuttondebug").attr("class", "btn btn-default active");
+});
