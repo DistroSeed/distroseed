b9e66b289d81a0dd8b9ed044fa885ee84caf933b
ltdenard <ltdenard@gmail.com>
2025-03-21 17:11:48 -0400

    adding in the updates

--- distroseed/distroseed/settings.py
+++ distroseed/distroseed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1:8000","127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
