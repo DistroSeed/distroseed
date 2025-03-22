66931920858fba7f5dbbb50856404bb0384b954d
ltdenard <ltdenard@gmail.com>
2025-03-21 21:52:58 -0400

    adding in views update

--- distroseed/dashboard/views.py
+++ distroseed/dashboard/views.py
@@ -74,7 +74,7 @@ def index(request):
         elif 'debian' in name.lower():
             distro = "Debian"
         elif 'mint' in name.lower() or 'lmde' in name.lower():
-            distro = "Linux Mint"
+            distro = "Linux_Mint"
         elif 'tails' in name.lower():
             distro = "Tails"
         elif 'opensuse' in name.lower():
